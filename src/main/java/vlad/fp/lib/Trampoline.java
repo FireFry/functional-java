ec471ab04b8b05c64eb05fca23ebfdf05090c312
FireFry <v.a.burakov@gmail.com>
2017-04-12 21:12:19 +1000

    Review and refacor Trampoline

--- src/main/java/vlad/fp/lib/Trampoline.java
+++ src/main/java/vlad/fp/lib/Trampoline.java
@@ -6,29 +6,33 @@ import vlad.fp.lib.function.Supplier;
 public final class Trampoline<T> {
   private final Free<Supplier, T> free;
 
+  private static <T> Trampoline<T> Trampoline(Free<Supplier, T> free) {
+    return new Trampoline<>(free);
+  }
+
   private Trampoline(Free<Supplier, T> free) {
     this.free = free;
   }
 
   public static <T> Trampoline<T> done(T value) {
-    return new Trampoline<>(Free.done(value));
+    return Trampoline(Free.done(value));
   }
 
   public static <T> Trampoline<T> suspend(Supplier<Trampoline<T>> thunk) {
     Supplier<Free<Supplier, T>> functor = () -> thunk.apply().free;
-    return new Trampoline<>(Free.suspend(functor));
+    return Trampoline(Free.suspend(functor));
   }
 
   public static <T> Trampoline<T> delay(Supplier<T> supplier) {
-    return new Trampoline<>(Free.liftF(Supplier.FUNCTOR, supplier));
+    return Trampoline(Free.liftF(Supplier.FUNCTOR, supplier));
   }
 
   public <R> Trampoline<R> flatMap(Function<T, Trampoline<R>> f) {
-    return new Trampoline<>(free.map(f).flatMap(o -> o.free));
+    return Trampoline(free.map(f).flatMap(o -> o.free));
   }
 
   public <R> Trampoline<R> map(Function<T, R> f) {
-    return new Trampoline<>(free.flatMap(t -> Free.done(f.apply(t))));
+    return Trampoline(free.flatMap(t -> Free.done(f.apply(t))));
   }
 
   public T run() {
