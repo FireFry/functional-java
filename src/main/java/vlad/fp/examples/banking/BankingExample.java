e71d9a59ef53cf1e3e50b5edb9bf8850f651f508
FireFry <v.a.burakov@gmail.com>
2017-04-13 23:56:26 +1000

    Extract ModelFactory

--- src/main/java/vlad/fp/examples/banking/BankingExample.java
+++ src/main/java/vlad/fp/examples/banking/BankingExample.java
@@ -1,15 +1,18 @@
 package vlad.fp.examples.banking;
 
+import static vlad.fp.examples.banking.dsl.model.ModelFactory.account;
+import static vlad.fp.examples.banking.dsl.model.ModelFactory.amount;
+import static vlad.fp.examples.banking.dsl.model.ModelFactory.from;
+import static vlad.fp.examples.banking.dsl.model.ModelFactory.to;
+
 import vlad.fp.examples.banking.dsl.banking.Banking;
 import vlad.fp.examples.banking.dsl.banking.BankingF;
-import vlad.fp.examples.banking.dsl.model.Account;
 import vlad.fp.examples.banking.dsl.model.Amount;
-import vlad.fp.examples.banking.dsl.model.From;
-import vlad.fp.examples.banking.dsl.model.To;
 import vlad.fp.examples.banking.exec.ExecBanking;
 import vlad.fp.lib.Free;
 import vlad.fp.lib.Monad;
 import vlad.fp.lib.Task;
+import vlad.fp.lib.function.Function;
 import vlad.fp.lib.higher.Parametrized;
 
 public class BankingExample {
@@ -17,8 +20,8 @@ public class BankingExample {
     return m.flatMap(
         banking.accounts(), as -> m.flatMap(
         banking.balance(as.get(0)), b -> m.flatMap(
-        banking.transfer(new Amount(123), new From(new Account("Foo")), new To(new Account("Bar"))), x -> m.map(
-        banking.withdraw(new Amount(5)), ignored -> b))));
+        banking.transfer(amount(123), from(account("Foo")), to(account("Bar"))), x -> m.map(
+        banking.withdraw(amount(5)), Function.identity()))));
   }
 
   private static Free<BankingF, Amount> bankingFProgram() {
--- src/main/java/vlad/fp/examples/banking/interpreters/BankingToProtocol.java
+++ src/main/java/vlad/fp/examples/banking/interpreters/BankingToProtocol.java
@@ -1,10 +1,11 @@
 package vlad.fp.examples.banking.interpreters;
 
+import static vlad.fp.examples.banking.dsl.model.ModelFactory.account;
+import static vlad.fp.examples.banking.dsl.model.ModelFactory.amount;
+import static vlad.fp.examples.banking.dsl.model.ModelFactory.error;
+
 import com.google.common.collect.ImmutableList;
 import vlad.fp.examples.banking.dsl.banking.BankingF;
-import vlad.fp.examples.banking.dsl.model.Account;
-import vlad.fp.examples.banking.dsl.model.Amount;
-import vlad.fp.examples.banking.dsl.model.Error;
 import vlad.fp.examples.banking.dsl.model.TransferResult;
 import vlad.fp.examples.banking.dsl.protocol.ProtocolF;
 import vlad.fp.lib.Either;
@@ -18,10 +19,10 @@ public enum BankingToProtocol implements Interpreter<BankingF, ProtocolF> {
   @Override
   public <T> Free<ProtocolF, T> apply(Parametrized<BankingF, T> fa) {
     return BankingF.lift(fa).foldT(
-        accounts -> ProtocolF.justReturn(accounts.next.apply(ImmutableList.of(new Account("Foo"), new Account("Bar")))),
-        balance -> ProtocolF.justReturn(balance.next.apply(new Amount(10000))),
-        transfer -> ProtocolF.justReturn(transfer.next.apply(new TransferResult(Either.left(new Error("Ooops"))))),
-        withdraw -> ProtocolF.justReturn(withdraw.next.apply(new Amount(10000 - withdraw.amount.value)))
+        accounts -> ProtocolF.justReturn(accounts.next.apply(ImmutableList.of(account("Foo"), account("Bar")))),
+        balance -> ProtocolF.justReturn(balance.next.apply(amount(10000))),
+        transfer -> ProtocolF.justReturn(transfer.next.apply(new TransferResult(Either.left(error("Ooops"))))),
+        withdraw -> ProtocolF.justReturn(withdraw.next.apply(amount(10000 - withdraw.amount.value)))
     );
   }
 }
