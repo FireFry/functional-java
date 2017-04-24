81600895bb17be15008a39b90c85b36d87aba69a
Vladyslav Burakov <vladyslav@canva.com>
2017-04-24 21:15:10 +1000

    Solve second Haskell 99 problem

--- src/test/java/vlad/fp/ninety_nine/HaskellNinetyNine.java
+++ src/test/java/vlad/fp/ninety_nine/HaskellNinetyNine.java
@@ -24,12 +24,32 @@ public class HaskellNinetyNine {
                         Matcher::unmatched,
                         (head, tail) -> tail.match(
                                 nil -> TailRec.done(head),
-                                cons -> TailRec.suspend(() -> lastTailRec(cons))
-                        )
-                );
+                                cons -> TailRec.suspend(() -> lastTailRec(cons))));
             }
 
         }.lastTailRec(list).eval();
+    }
+
+    @Test
+    public void problem2() {
+        assertEquals(3, (int) butLast(List.of(1, 2, 3, 4)));
+        assertEquals('y', (char) butLast(List.of('x', 'y', 'z')));
+    }
+
+    private static <A> A butLast(List<A> list) {
+        return new NestedFunction() {
+
+            <A> TailRec<A> butLastTailRec(List<A> list) {
+                return list.matchVal(
+                        Matcher::unmatched,
+                        (x, tail) -> tail.matchVal(
+                                Matcher::unmatched,
+                                (y, tail2) -> tail2.match(
+                                        nil -> TailRec.done(x),
+                                        cons -> TailRec.suspend(() -> butLastTailRec(tail)))));
+            }
+
+        }.butLastTailRec(list).eval();
     }
 
 }
